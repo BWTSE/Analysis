a8386ffc42274e87573c85d7fffb40aa635b8912
williamleven <william@leven.id>
2021-03-21 11:15:13 +0100

    initial model

--- model_documentation.R
+++ model_documentation.R
@@ -1,8 +1,8 @@
 library(brms)
 
 doc0 <- brm(
-  O_documentation_c3 ~ 1,
-  # + was_high_debt_b
+  O_documentation_oc3 ~ 1
+  + was_high_debt_b
   # + mo(education_level_oc7)
   # + education_field_c2
   # + work_exp_field_c6
@@ -14,28 +14,20 @@ doc0 <- brm(
   # + wp_coding_stds_b
   # + scenario_c2
   # + mo(sys_qual_score)
-  # + (1 | id),
-  family = dirichlet(),
+  + (1 | id),
+  family = cumulative(),
   data = d,
-  # prior = c(
-  #   # prior(normal(0, 0.1), class = "b"),
-  #   # prior(normal(0, 1), class = "Intercept"),
-  #   # prior(exponential(1), class = "sd", resp = Oreusedlogicconstructorb),
-  #   # prior(exponential(1), class = "sd", resp = Oreusedlogicequalsb),
-  #   # prior(exponential(1), class = "sd", resp = Oreusedlogichashcodeb),
-  #   # prior(exponential(1), class = "sd", resp = Oreusedlogicvalidationb),
-  #   
-  #   # prior(dirichlet(2), class = "simo", coef = "moeducation_level_oc71", resp = Oreusedlogicconstructorb),
-  #   # prior(dirichlet(2), class = "simo", coef = "moeducation_level_oc71", resp = Oreusedlogicequalsb),
-  #   # prior(dirichlet(2), class = "simo", coef = "moeducation_level_oc71", resp = Oreusedlogichashcodeb),
-  #   # prior(dirichlet(2), class = "simo", coef = "moeducation_level_oc71", resp = Oreusedlogicvalidationb),
-  #   
-  #   # prior(dirichlet(2), class = "simo", coef = "mosys_qual_score1", resp = Oreusedlogicconstructorb),
-  #   # prior(dirichlet(2), class = "simo", coef = "mosys_qual_score1", resp = Oreusedlogicequalsb),
-  #   # prior(dirichlet(2), class = "simo", coef = "mosys_qual_score1", resp = Oreusedlogichashcodeb),
-  #   # prior(dirichlet(2), class = "simo", coef = "mosys_qual_score1", resp = Oreusedlogicvalidationb),
-  #   # prior(lkj(1), class = "L")
-  # ),
+  prior = c(
+    prior(normal(0, 0.1), class = "b"),
+    prior(normal(0, 1), class = "Intercept"),
+    prior(exponential(1), class = "sd")
+
+    # prior(dirichlet(2), class = "simo", coef = "moeducation_level_oc71"),
+    # 
+    # prior(dirichlet(2), class = "simo", coef = "mosys_qual_score1")
+    
+    # prior(lkj(1), class = "L")
+  ),
   # empty = TRUE, # If you only want to check prior definitions
   # sample_prior = "only",
   backend = "cmdstanr",
